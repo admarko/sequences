9628b1ace52aa2e81fdfd2a47c6edb90f1dd1f38
Alex Markowitz <admarko@gmail.com>
2017-01-31 21:11:03 -0600

    64%

--- app/controllers/puzzle_controller.rb
+++ app/controllers/puzzle_controller.rb
@@ -1,11 +1,19 @@
 class PuzzleController < ApplicationController
 
   def answers
-
+    @rule = params["rule"]
     render("/answers.html.erb")
   end
 
   def guesses
+    @num1 = params["first_number"].to_i
+    @num2 = params["second_number"].to_i
+    @num3 = params["third_number"].to_i
+
+    @result = "NO"
+    if @num1 < @num2 && @num2 < @num3
+      @result = "YES"
+    end
 
     render("/guesses.html.erb")
   end
